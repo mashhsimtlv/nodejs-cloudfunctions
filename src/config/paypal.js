c455cc0e526ab4a7da4461d1570c8791f607b2c9
mashhoodrehman <mashhoodr.rehman@gmail.com>
2025-09-02 18:01:03 +0500

    Paypal now integrated

--- src/config/paypal.js
+++ src/config/paypal.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 async function getPayPalAccessToken() {
     const response = await axios({
         method: "post",
-        url: "https://api-m.paypal.com/v1/oauth2/token",
+        url: "https://api.sandbox.paypal.com/v1/oauth2/token",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         auth: {
             username: process.env.PAYPAL_CLIENT_ID,
