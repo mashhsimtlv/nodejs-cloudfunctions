24eaea3be4535650001e0311229b5a08137737ed
mashhoodrehman <mashhoodr.rehman@gmail.com>
2026-04-02 15:20:25 +0500

    Get All Conversations webhook

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -76,6 +76,7 @@ class PaymentService {
             || email.toLowerCase().includes("adirojak883@gmail.com")
             || email.toLowerCase().includes("zeaardelia9@gmail.com")
             || email.toLowerCase().includes("reada1370@gmail.com")
+            || email.toLowerCase().includes("barubaru45600@gmail.com")
         ) {
             //if (email.toLowerCase().includes("@gmail.com")) {
             console.log("Blocked payment intent for boticuk.com domain:", email);
